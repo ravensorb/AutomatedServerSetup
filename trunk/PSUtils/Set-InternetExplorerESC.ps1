9337d499b0e59c70283fd2d129690733dc8d0b56
ravensorb_cp <SND\ravensorb_cp>
2014-09-11 12:45:30 +0000

    Fixed a bug where the changes where not reflected immediately

--- trunk/PSUtils/Set-InternetExplorerESC.ps1
+++ trunk/PSUtils/Set-InternetExplorerESC.ps1
@@ -2,31 +2,31 @@ Function Set-InternetExplorerESC {
 <#     
 .SYNOPSIS     
    Function used to set the Internet Explorer Enhanced Security Settings
-     
+	 
 .DESCRIPTION   
    Function used to set the Internet Explorer Enhanced Security Settings
-      
+	  
 .NOTES     
-    Name: Set-InternetExplorerESC
-    Author: Shawn Anderson
-    DateCreated: 6December2013    
-      
+	Name: Set-InternetExplorerESC
+	Author: Shawn Anderson
+	DateCreated: 6December2013    
+	  
 .EXAMPLE    
 	Enable ESC for both Administrators and Users
 	
-    [PS] C:\> Set-InternetExplorerESC -Admin $True -User $True
+	[PS] C:\> Set-InternetExplorerESC -Admin $True -User $True
 .EXAMPLE     
 	Disable ESC for only Administrators
 
 	[PS] C:\> Set-InternetExplorerESC -Admin $False
-      
+	  
    
 Description   
 -----------       
 Command will allow for enabling/disabling Internet Explorer Enhanced Security Checks 
 #>  
-    [cmdletbinding()]  
- 	param(
+	[cmdletbinding()]  
+	param(
 		[Boolean]$Admin = $false,
 		[Boolean]$User = $true
 	)
@@ -48,5 +48,9 @@ Command will allow for enabling/disabling Internet Explorer Enhanced Security Ch
 	$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
 	Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value $User
 	Write-Host "IE Enhanced Security Configuration (ESC) has been" (?: {$User} {"enabled"} {"disabled"}) "for Users." -ForegroundColor Green
-    
+	
+	Write-Verbose ('Calling iesetup.dll hardening methods.')
+	Rundll32 iesetup.dll, IEHardenLMSettings
+	Rundll32 iesetup.dll, IEHardenUser
+	Rundll32 iesetup.dll, IEHardenAdmin 
 }
